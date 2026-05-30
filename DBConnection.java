c288505d81b18a7ec0fad7d1d9d184ba62722690
edorotpatrickjhon <edorotpatrickjhon@gmail.com>
2026-05-30 22:26:18 +0800

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,19 @@
+package com.mycompany.studentmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection connect() {
+        try {
+            return DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/studentdb",
+                "postgres",
+                "YOUR_PASSWORD"
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
