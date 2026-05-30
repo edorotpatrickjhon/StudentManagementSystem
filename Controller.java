d0d0179993495efd7b93bf3b0c8846cf1c1c839c
edorotpatrickjhon <edorotpatrickjhon@gmail.com>
2026-05-30 22:27:59 +0800

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,20 @@
+package com.mycompany.studentmanagementsystem;
+
+public class Controller {
+
+    public void addStudent() {
+        System.out.println("Add Student");
+    }
+
+    public void updateStudent() {
+        System.out.println("Update Student");
+    }
+
+    public void deleteStudent() {
+        System.out.println("Delete Student");
+    }
+
+    public void clearFields() {
+        System.out.println("Clear Fields");
+    }
+}
