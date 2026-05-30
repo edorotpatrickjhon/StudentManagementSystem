e3ff747dbd473b6bf0b4120f065bef9245da56e6
edorotpatrickjhon <edorotpatrickjhon@gmail.com>
2026-05-30 22:34:45 +0800

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+
     public static Connection connect() {
         try {
             return DriverManager.getConnection(
